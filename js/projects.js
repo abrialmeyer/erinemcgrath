adbf886ca60b078aea76c2766f3fe1f4ca0847e0
ammeyer <ammeyer@wesleyan.edu>
2018-03-05 17:32:08 -0500

    first

---
+++ js/projects.js
@@ -0,0 +1,90 @@
+var projects = { 
+	types : [
+		"food", "outdoors", "art", "academic"
+	],
+	about : {
+		text : [ "Hey!", "I do fun things."],
+	},
+	main : [
+		{
+			title : "Music",
+			type : "food",
+			description : [""],
+			haslink : true,
+			links : ["https://dignitty.bandcamp.com/"],
+			images : ["images/earthquake/earthquake1.png", "images/earthquake/earthquake2.png"],
+			vimeo : [],
+			youtube : ["https://www.youtube.com/embed/FPYpl-aGtCw"]
+		},
+		{
+			title : "Fermentation",
+			type : "food",
+			description : [],
+			haslink : true,
+			links : [""],
+			images : ["images/round/round01.jpg", "images/round/round02.png", "images/round/round05.png", "images/round/round06.png", "images/round/round07.png"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Printmaking",
+			type : "art",
+			description : [],
+			haslink : false,
+			links : [""],
+			images : ["images/doit/doit01.png", "images/doit/doit02.png", "images/doit/doit03.png"],
+			vimeo : ["https://player.vimeo.com/video/128102510?byline=0&portrait=0"],
+			youtube : []
+		},
+		{
+			title : "Crafting",
+			type : "art",
+			description : [],
+			haslink : true,
+			links : [""],
+			images : [],
+			vimeo : [],
+			youtube : ["https://www.youtube.com/embed/kd1gjUt7gI0"]
+		},
+		{
+			title : "Pacific Crest Trail",
+			type : "outdoors",
+			description : [],
+			haslink : true,
+			links : [""],
+			images : ["images/headhaiku/headhaiku.gif"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Sustainability",
+			type : "academic",
+			description : [],
+			haslink : false,
+			links : [""],
+			images : [],
+			vimeo : ["https://player.vimeo.com/video/128754679?title=0&byline=0&portrait=0"],
+			youtube : []
+		},
+		{
+			title : "Teaching",
+			type : "academic",
+			description : ["AMNH and wes (marine bio and cs ta"],
+			haslink : false,
+			links : [""],
+			images : ["images/segue/segue1.png", "images/segue/segue2.png", "images/segue/segue3.png"],
+			vimeo : [],
+			youtube : []
+		},
+		{
+			title : "Senior Essay",
+			type : "interactive",
+			description : [],
+			haslink : false,
+			links : [""],
+			images : ["images/sunglasses/sunglasses.gif"],
+			vimeo : [],
+			youtube : ["https://www.youtube.com/embed/0c87tY9SYs4"]
+		}
+	]
+}
